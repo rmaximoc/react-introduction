6c485bb250c3dab58ccd3e1afe70d061000532da
rmaximoc <rodrigomaximocano@gmail.com>
2019-10-24 11:44:02 -0300

    Add image loaders

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
+import "./App.css";
+import TechList from "./components/TechList";
 
 function App() {
-  return <h1>hello</h1>;
+  return <TechList />;
 }
 
 export default App;
---
+++ src/components/TechList.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class TechList extends Component {
+  render() {
+    return (
+      <ul>
+        <li>primeiro</li>
+        <li>segundo</li>
+        <li>terceiro</li>
+      </ul>
+    );
+  }
+}
+
+export default TechList;
